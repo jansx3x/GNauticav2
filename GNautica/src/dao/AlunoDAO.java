c58c905691f3f31fa8385ba1be843dfb3835858b
Samantha <samanthanfer@gmail.com>
2017-09-07 13:27:15 -0300

    AlunoDAO

--- GNautica/src/dao/AlunoDAO.java
+++ GNautica/src/dao/AlunoDAO.java
@@ -1,4 +1,8 @@
 package dao;
+import factory.ConnectionFactory;
+import modelo.Aluno;
+import java.sql.*;
+import java.sql.PreparedStatement;
 
 /**
  *
@@ -6,5 +10,22 @@ package dao;
  */
 
 public class AlunoDAO {
+    private Connection connection;
+    int idAluno;
+    String nome;
+    String cpf;
+    String endereco;
+    String telefone;
+    String email;
+    String categoria;
+    String pendencia;
+    String avaliacao;
     
+    public AlunoDAO(){
+        this.connection = new ConnectionFactory().getConnection();
+    }
+    
+    public void Adiciona(Aluno aluno){
+        
+    }
 }
